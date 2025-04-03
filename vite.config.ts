de7a9eb41171769a34e7687c381be4e36ec4af46
stefanhaindl <129992890+stefwh@users.noreply.github.com>
2025-04-03 14:51:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/band-landing-page",
+  base: "/band-landing-page/",
   server: {
     host: "::",
     port: 8080,
